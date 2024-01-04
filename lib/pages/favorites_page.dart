b9d20582bd80b86ed809b8a5fbe33f37b24aa3fb
taskoft <thief811@gmail.com>
2024-01-04 16:35:05 +0300

    NavigationBar State Düzeltme

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -2,9 +2,7 @@ import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 import "package:trendyol_store/blocs/product_bloc/product_bloc.dart";
 import "package:trendyol_store/models/product.dart";
-import "package:trendyol_store/pages/home_page.dart";
 import "package:trendyol_store/widgets/card.dart";
-import '../widgets/app_bar.dart';
 
 // ignore: must_be_immutable
 class FavoritesPage extends StatefulWidget {
@@ -19,7 +17,6 @@ class _FavoritesPageState extends State<FavoritesPage> {
   final bool isChecked = false;
   late List<int> favoritesIdList;
   bool here = false;
-
   @override
   void initState() {
     favoritesIdList = widget.favoritesIdList;
@@ -34,69 +31,39 @@ class _FavoritesPageState extends State<FavoritesPage> {
     final productBloc = BlocProvider.of<ProductListBloc>(context);
     // productBloc.add(FetchProductsEvent());
     List<Product> favoritedProductList = <Product>[];
-    return Scaffold(
-      backgroundColor: Colors.grey.shade200,
-      appBar: const PreferredSize(
-          preferredSize: Size(100, 45), child: AppBarWidget()),
-      body: BlocBuilder(
-        bloc: productBloc,
-        builder: (context, state) {
-          if (state is ProductListLoadedState && currentIndex == 2) {
-            final List<Product> products = state.products;
-            for (var f in favoritesIdList) {
-              favoritedProductList += (products.where(
-                (element) => f == element.id,
-              )).toList();
-            }
+    return BlocBuilder(
+      bloc: productBloc,
+      builder: (context, state) {
+        if (state is ProductListLoadedState && currentIndex == 2) {
+          final List<Product> products = state.products;
+          for (var f in favoritesIdList) {
+            favoritedProductList += (products.where(
+              (element) => f == element.id,
+            )).toList();
+          }
 
-            if (products.isEmpty) {
-              return const Text("Products is null");
-            }
-            return GridView.builder(
-                itemCount: favoritedProductList.length,
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 270, mainAxisExtent: 250),
-                itemBuilder: (BuildContext context, int count) {
-                  return CardWidget(
-                    productImagePath:
-                        favoritedProductList[count].image.toString(),
-                    productName: favoritedProductList[count].title.toString(),
-                    productId: favoritedProductList[count].id!,
-                    addFavoriteId: (int id, bool isChecked) {},
-                  );
-                });
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
+          if (products.isEmpty) {
+            return const Text("Products is null");
           }
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Colors.black,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.orange.shade600,
-        backgroundColor: Colors.grey.shade100,
-        currentIndex: currentIndex,
-        iconSize: 35,
-        onTap: (int newIndex) {
-          currentIndex = newIndex;
-          if (newIndex == 0) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-            );
-          }
-        },
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_basket), label: ""),
-        ],
-      ),
+          return GridView.builder(
+              itemCount: favoritedProductList.length,
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 270, mainAxisExtent: 250),
+              itemBuilder: (BuildContext context, int count) {
+                return CardWidget(
+                  productImagePath:
+                      favoritedProductList[count].image.toString(),
+                  productName: favoritedProductList[count].title.toString(),
+                  productId: favoritedProductList[count].id!,
+                  addFavoriteId: (int id, bool isChecked) {},
+                );
+              });
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,74 +7,105 @@ import "package:trendyol_store/widgets/card.dart";
 import '../widgets/app_bar.dart';
 
 // ignore: must_be_immutable
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<int> _favoriteIdList = [];
+  final PageController _pageController = PageController();
   int _currentIndex = 0;
+  bool dataFetched = false;
   @override
   Widget build(BuildContext context) {
     final productBloc = BlocProvider.of<ProductListBloc>(context);
-    productBloc.add(FetchProductsEvent());
+    if (!dataFetched) {
+      productBloc.add(FetchProductsEvent());
+      dataFetched = true;
+    }
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       appBar: const PreferredSize(
           preferredSize: Size(100, 45), child: AppBarWidget()),
-      body: BlocBuilder(
-        bloc: productBloc,
-        builder: (context, state) {
-          if (state is ProductListLoadedState && _currentIndex == 0) {
-            final List<Product> products = state.products;
-            if (products.isEmpty) {
-              return const Text("Products is null");
-            }
-            return GridView.builder(
-                itemCount: products.length,
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 270, mainAxisExtent: 250),
-                itemBuilder: (BuildContext context, int count) {
-                  return CardWidget(
-                    productImagePath: products[count].image.toString(),
-                    productName: products[count].title.toString(),
-                    productId: products[count].id!,
-                    addFavoriteId: (int id, bool isChecked) {
-                      if (isChecked) {
-                        _favoriteIdList.add(id);
-                      } else {
-                        _favoriteIdList.remove(id);
-                      }
-                    },
-                  );
-                });
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
         },
+        children: [
+          BlocBuilder(
+            bloc: productBloc,
+            builder: (context, state) {
+              if (state is ProductListLoadedState && _currentIndex == 0) {
+                final List<Product> products = state.products;
+                if (products.isEmpty) {
+                  return const Text("Products is null");
+                }
+                return GridView.builder(
+                    itemCount: products.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 270, mainAxisExtent: 250),
+                    itemBuilder: (BuildContext context, int count) {
+                      return CardWidget(
+                        productImagePath: products[count].image.toString(),
+                        productName: products[count].title.toString(),
+                        productId: products[count].id!,
+                        addFavoriteId: (int id, bool isChecked) {
+                          if (isChecked) {
+                            _favoriteIdList.add(id);
+                          } else {
+                            _favoriteIdList.remove(id);
+                          }
+                        },
+                      );
+                    });
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+          const Text(""),
+          FavoritesPage(favoritesIdList: _favoriteIdList),
+          const Text(""),
+        ],
       ),
-      bottomNavigationBar: BottomNavigationBar(unselectedItemColor: Colors.black,
-       type: BottomNavigationBarType.fixed,
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
         selectedItemColor: Colors.orange.shade600,
         backgroundColor: Colors.grey.shade100,
         currentIndex: _currentIndex,
         iconSize: 35,
         onTap: (int newIndex) {
           _currentIndex = newIndex;
-          if (newIndex == 2) {
-            Navigator.push(
+          _pageController.animateToPage(newIndex,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut);
+
+          /* if (newIndex == 2) {
+
+             Navigator.push(
                 context,
                 MaterialPageRoute(
                     builder: (context) => FavoritesPage(
                           favoritesIdList: _favoriteIdList,
                         )));
-          }
-        },showSelectedLabels: false,
+          }*/
+        },
+        showSelectedLabels: false,
         showUnselectedLabels: false,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ""),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_basket), label: ""),
         ],
       ),
